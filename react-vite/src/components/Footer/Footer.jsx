a4d5f335827743282c6fdc4e1f7b814c5518c0ac
Brandon <myboyknuckles@gmail.com>
2024-05-24 07:20:59 -0500

    Added links to footer

--- react-vite/src/components/Footer/Footer.jsx
+++ react-vite/src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
+import { IoLogoGithub } from "react-icons/io";
+import { BsLinkedin } from "react-icons/bs";
 import "./Footer.css"
 
 export default function Footer() {
-  return (<div className="footer">
-    <div className="container">
-      <span>Website Developed by Kiante Moore</span>
-      <div></div>
+  return (
+    <div className="footer">
+      <div className="container">
+        <div>Website Developed by Kiante Moore</div>
+        <div className="contacts">
+            <a
+              href="https://github.com/BrandonKMoore"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-link"
+            >
+              <IoLogoGithub className="github-logo" />
+              <span>GitHub</span>
+            </a>
+            <a
+              href="https://linkedin.com/in/kiante-moore-779250101"
+              target="_blank"
+              rel="noreferrer"
+              className="footer-link"
+            >
+              <BsLinkedin className="linkedin-logo" />
+              <span>LinkedIn</span>
+            </a>
+          </div>
+      </div>
     </div>
-  </div>)
+  )
 }
