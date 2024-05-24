0a403613419abde1e545810c6fbac04b3b4bf426
Brandon <myboyknuckles@gmail.com>
2024-05-24 15:06:24 -0500

    npm build debug

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -3,7 +3,7 @@ import LoginFormModal from '../LoginFormModal'
 import SignupFormModal from "../SignupFormModal";
 import "./Navigation.css";
 import { IoPersonOutline } from "react-icons/io5";
-import { PiShoppingCartSimple  } from "react-icons/pi";
+// import { PiShoppingCartSimple  } from "react-icons/pi";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
